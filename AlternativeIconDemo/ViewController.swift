dd9c8984eecef1b4449e13bf7202a86d5114cd82
hellowupeng <hellowupeng@gmail.com>
2017-04-16 11:03:00 +0800

    changing icon dynamicly

--- AlternativeIconDemo/ViewController.swift
+++ AlternativeIconDemo/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	// MARK: life cycle
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,22 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	// MARK: action
+	
+	@IBAction func changeIcon(_ sender: UIButton) {
+		let application = UIApplication.shared
+		if application.supportsAlternateIcons {
+			let num = arc4random_uniform(2) + 1
+			let alternateIconName = "Test\(num)"
+			
+			print("alternate icon name --- \(alternateIconName)")
+			UIApplication.shared.setAlternateIconName(alternateIconName, completionHandler: {error in
+				if let err = error {
+					print("error --- \(err.localizedDescription)")
+				}
+			})
+		}
+	}
 
 }
 
